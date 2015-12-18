fb850d2adc238da605f1935106ddb5457b9d48a2
Harsh Overseer <harsh@embry.io>
2015-12-18 16:09:34 +1100

    updated script to include a custom port number and messages to warn sending of an event

--- cefGen.py
+++ cefGen.py
@@ -7,9 +7,8 @@ class cefGen(object):
 		if not filename:
 			self.readFile()
 
-	def outputWrite(self, output):
-                TCP_PORT = 513
-                print TCP_PORT
+	def outputWrite(self, output, port):
+		TCP_PORT = int(port)
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 s.connect(('', TCP_PORT))
                 s.send(output)
@@ -18,16 +17,17 @@ class cefGen(object):
 	def readFile(self):
 		path = os.getcwd()
 		print "path is " + path
-		filename = raw_input("What is the filename?")
+		print "Today we'll be generating a 0.33 EPS load of CEF events to a TCP socket on this host!"
+		port = raw_input("What port would you like to send this load to? Remember this value can only be between 0-65535, preferably higher than 1024! ")
+		filename = raw_input("What is the filename? Remember, the file should be in the same directory as this script! ")
 		filename = path + "/" + filename
 		cefFile = open(filename)
 		for line in cefFile.readlines():
-			print "about to print line"
+			print "about to print event"
 			time.sleep(3)
 			print line
-			print "Sending event through to LS"
-			self.outputWrite(line)
-			time.sleep(3)
+			print "Sending event through to TCP port " + port
+			self.outputWrite(line, port)
 		f.close()
 
 def main():
